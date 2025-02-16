120c3936c74fea8a9a8929f1013b6e6e357137ed
ofithcheallaigh <sofithcheallaigh@gmail.com>
2025-02-16 21:12:02 +0000

    Deploying to gh-pages from @ ofithcheallaigh/ofithcheallaigh.github.io@1709d5f89bfb67b29a130a529714c6dcf7b6b1de 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -410,16 +410,13 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+    },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-this-is-my-new-site-and-it-is-very-much-under-development-and-i-do-have-one-or-two-other-things-going-on-so-it-might-be-developing-for-a-while",
+          title: 'This is my new site, and it is very much under development. And...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
